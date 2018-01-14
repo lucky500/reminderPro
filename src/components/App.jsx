b67988ea8dd8348e90d7d1032da3ca0c19b70022
Luciano Oliveira <luciano101@msn.com>
2018-01-13 18:27:09 -0800

    complete renderReminders method and add some styling

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -17,10 +17,28 @@ class App extends Component {
     this.props.addReminder(this.state.text)
   }
 
+  renderReminders(){
+    //we can use this syntax for our variable, when the variable name and its
+    //value is the same.
+    const { reminders } = this.props;
+    return (
+      <ul className="list-group col-sm-4">
+        {
+          reminders.map(reminder =>{
+            return (
+              <li key={reminder.id} className="list-group-item">
+                <div>{reminder.text}</div>
+              </li>
+            )
+          })
+        }
+      </ul>
+    )
+  }
+
   render(){
-    console.log('this.props', this.props);
     return(
-      <div className="app container">
+      <div className="app container reminder-form">
         <div className="title">
           <h3>ReminderPro</h3>
         </div>
@@ -36,8 +54,10 @@ class App extends Component {
           type="button" 
           className="btn btn-primary"
           onClick={() => this.addReminder()}
-          >Submit</button>
+          >Add Reminder</button>
+          { this.renderReminders() }
       </div>
+
     )
   }
 }
